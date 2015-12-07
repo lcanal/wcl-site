c5f7972c8add7acc2f9311b2c807d19ee8198d57
Luis Canales <cluchomucho@gmail.com>
2015-12-07 10:49:52 -0800

    Added testing for digital_ocean inventory files. Added use of user_data for coreos deployments Added donatello public key

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import re
+import argparse
+from time import time
+import ConfigParser
+from dopy.manager import DoError, DoManager
+
+print "Hello"
+print "Is it me you're looking for"
